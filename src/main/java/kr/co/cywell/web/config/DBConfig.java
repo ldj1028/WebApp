453b6a2778e3df7cf456680bf5e68de713d9391c
ldj1028 <ldj1028@gmail.com>
2023-02-07 16:00:04 +0900

    Session Modify

--- src/main/java/kr/co/cywell/web/config/DBConfig.java
+++ src/main/java/kr/co/cywell/web/config/DBConfig.java
@@ -8,9 +8,13 @@ import java.sql.SQLException;
 public class DBConfig {
     private boolean bOracle = false;
     private boolean bMaria = false;
+    private boolean bMSSQL = false;
+    private boolean bMySQL = false;
     public DBConfig() {
         bOracle = (System.getProperty("webapp.datasource.oracle") != null)?true:false;
         bMaria = (System.getProperty("webapp.datasource.maria") != null)?true:false;
+        bMSSQL = (System.getProperty("webapp.datasource.mssql") != null)?true:false;
+        bMySQL = (System.getProperty("webapp.datasource.mysql") != null)?true:false;
     }
 
     public String connectionDB() throws SQLException{
@@ -23,6 +27,14 @@ public class DBConfig {
         if(bMaria){
             bSuccess = connMaria();
             msg += "Maria DB Connection : " + bSuccess +"</br>";
+        }
+        if(bMSSQL){
+            bSuccess = connMSSQL();
+            msg += "MSSQL DB Connection : " + bSuccess +"</br>";
+        }
+        if(bMySQL){
+            bSuccess = connMySQL();
+            msg += "MySQL DB Connection : " + bSuccess +"</br>";
         }
         return msg;
     }
@@ -55,6 +67,46 @@ public class DBConfig {
             DataSource ds = (DataSource) ic.lookup(dsName);
             conn = ds.getConnection();
             System.out.println("Maria DB Connection Successful!!!");
+            System.out.println("Conn : " + conn);
+            conn.close();
+            bSuccess = true;
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally {
+            if(conn != null ) conn.close();
+        }
+        return bSuccess;
+    }
+
+    private boolean connMSSQL() throws SQLException{
+        String dsName = System.getProperty("webapp.datasource.mssql");
+        Connection conn = null;
+        boolean bSuccess = false;
+        try{
+            InitialContext ic = new InitialContext();
+            DataSource ds = (DataSource) ic.lookup(dsName);
+            conn = ds.getConnection();
+            System.out.println("MSSQL DB Connection Successful!!!");
+            System.out.println("Conn : " + conn);
+            conn.close();
+            bSuccess = true;
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally {
+            if(conn != null ) conn.close();
+        }
+        return bSuccess;
+    }
+
+    private boolean connMySQL() throws SQLException{
+        String dsName = System.getProperty("webapp.datasource.mysql");
+        Connection conn = null;
+        boolean bSuccess = false;
+        try{
+            InitialContext ic = new InitialContext();
+            DataSource ds = (DataSource) ic.lookup(dsName);
+            conn = ds.getConnection();
+            System.out.println("MySQL DB Connection Successful!!!");
             System.out.println("Conn : " + conn);
             conn.close();
             bSuccess = true;
--- src/main/java/kr/co/cywell/web/controller/SessionController.java
+++ src/main/java/kr/co/cywell/web/controller/SessionController.java
@@ -11,14 +11,16 @@ public class SessionController {
     @GetMapping("/session")
     public String cywellSession(HttpSession session) {
         Integer index = (Integer)session.getAttribute("session_counter");
-
+        String msg = "";
         if(index == null) {
             index = 1;
         }else {
             index = index.intValue() + 1;
         }
         session.setAttribute("session_counter", index);
-        System.out.println("here!!!!!!");
+        msg += "Session Counter : " + session.getAttribute("session_counter") + "</br>";
+        msg += "Session ID : " + session.getId() + "</br>";
+        msg += "Session Creation Time : " + session.getCreationTime() + "</br>";
         return "Session Counter : " + session.getAttribute("session_counter");
     }
 
